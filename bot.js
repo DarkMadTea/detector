a6c3c6d7d3f54ac6367e7cf370d5e7e5b06d925c
Vladislav <ducknuk.em13@gmail.com>
2021-01-30 21:14:20 +0300

    1

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ let fs = require('fs');
 const robot = new Discord.Client();
 let config = require('./config.json');
 let prefix = config.prefix;
+let request = require('request');
 
 robot.on('guildMemberAdd', (member) =>{
     if (member.guild.id === '503098565485133834'){
@@ -30,70 +31,58 @@ robot.on('message', msg => {
     let bratvaID = "311147386225295361";
 
     let usersOfServer = 'users.txt';
-
     if (msg.guild.id === bratvaID){
         usersOfServer = 'users1.txt'
     }
-    var array = fs.readFileSync(usersOfServer).toString().split("\n");
-    let item = array[Math.floor(Math.random()*array.length)];
-
-    let roleID = "797546408496332800";
 
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
-
     let randomMsg = getRandomInt(5);
 
     if (msg.content === prefix + 'ктопидор') {
+        request.get('https://raw.githubusercontent.com/DarkMadTea/usersListfordetector/main/' + usersOfServer, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var csv = body;
+                var array = fs.readFileSync(csv).toString().split("\n");
+                let item = array[Math.floor(Math.random()*array.length)];
 
-        if (randomMsg === 0){
-            msg.channel.send('Woob-woob, that\'s da sound of da pidor-police!');
-            msg.channel.send('Ведется поиск в базе данных');
-            msg.channel.send('Ведется захват подозреваемого');
-            msg.channel.send(`И прекрасный человек сегодня... а нет, ошибка, всего-лишь пидор - <@${item}>`);
-        }
+                if (randomMsg === 0){
+                    msg.channel.send('Woob-woob, that\'s da sound of da pidor-police!');
+                    msg.channel.send('Ведется поиск в базе данных');
+                    msg.channel.send('Ведется захват подозреваемого');
+                    msg.channel.send(`И прекрасный человек сегодня... а нет, ошибка, всего-лишь пидор - <@${item}>`);
+                }
 
-        if (randomMsg === 1){
-            msg.channel.send('Woob-woob, that\'s da sound of da pidor-police!');
-            msg.channel.send('Выезжаю на место...');
-            msg.channel.send('Но кто же он?');
-            msg.channel.send(`Пидор обыкновенный, 1шт. - <@${item}>`);
-        }
+                if (randomMsg === 1){
+                    msg.channel.send('Woob-woob, that\'s da sound of da pidor-police!');
+                    msg.channel.send('Выезжаю на место...');
+                    msg.channel.send('Но кто же он?');
+                    msg.channel.send(`Пидор обыкновенный, 1шт. - <@${item}>`);
+                }
 
-        if (randomMsg === 2){
-            msg.channel.send('Woob-woob, that\'s da sound of da pidor-police!');
-            msg.channel.send('Ведется погоня за пидорасом...');
-            msg.channel.send('https://cdn.discordapp.com/attachments/797547926137077760/804486649018712104/175145d20796c04748dfbaba5f8c3e890b5375b7.gif');
-            msg.channel.send(`Пидор пойман! Им оказался - <@${item}>`);
-        }
+                if (randomMsg === 2){
+                    msg.channel.send('Woob-woob, that\'s da sound of da pidor-police!');
+                    msg.channel.send('Ведется погоня за пидорасом...');
+                    msg.channel.send('https://cdn.discordapp.com/attachments/797547926137077760/804486649018712104/175145d20796c04748dfbaba5f8c3e890b5375b7.gif');
+                    msg.channel.send(`Пидор пойман! Им оказался - <@${item}>`);
+                }
 
-        if (randomMsg === 3){
-            msg.channel.send('Woob-woob, that\'s da sound of da pidor-police!');
-            msg.channel.send('Система поиска пидорасов активирована...');
-            msg.channel.send(`<@${item}>,`);
-            msg.channel.send('https://tenor.com/view/56324%D0%BF%D0%BA-cat-rest-chill-gif-15911415');
-        }
-        if (randomMsg === 4){
-            msg.channel.send('Инициирую поиск пидора дня...');
-            msg.channel.send('Машины выехали');
-            msg.channel.send('Так-так, что же тут у нас...');
-            msg.channel.send(`Вот ты и пидор, <@${item}>,`);
-        }
+                if (randomMsg === 3){
+                    msg.channel.send('Woob-woob, that\'s da sound of da pidor-police!');
+                    msg.channel.send('Система поиска пидорасов активирована...');
+                    msg.channel.send(`<@${item}>,`);
+                    msg.channel.send('https://tenor.com/view/56324%D0%BF%D0%BA-cat-rest-chill-gif-15911415');
+                }
+                if (randomMsg === 4){
+                    msg.channel.send('Инициирую поиск пидора дня...');
+                    msg.channel.send('Машины выехали');
+                    msg.channel.send('Так-так, что же тут у нас...');
+                    msg.channel.send(`Вот ты и пидор, <@${item}>,`);
+                }
 
-        // for (let i = 0; i < arr.length; i++){
-        //     if (msg.member.roles.cache.find(r => r.id === roleID)){
-        //         msg.member.roles.remove(roleID);
-        //         console.log("The role has beed removed")
-        //     }
-        // }
-
-        // if(msg.guild.members.cache.has(item)){
-        //     msg.guild.members.cache.get(item)
-        //         .roles.add(roleID);
-        //     console.log("the role has been added to <@"+item+">");
-        // }
-
+            }
+        });
     }
 
     if ( ((msg.content.match(/Выбил/)) || (msg.content.match(/выбил/))) && (msg.guild.id === nervID)){
@@ -129,14 +118,6 @@ robot.on('message', msg => {
             msg.channel.send('Let the cum begins!');
             msg.channel.send('https://tenor.com/view/gachi-gachi-hyper-gif-15959866');
         }, 1000);
-        // if(msg.member.voice.channel){
-        //  msg.member.voice.channel.join();
-            //let musicChannel = robot.channels.cache.find(channel => channel.name === "music-bots");
-            // musicChannel.send('-play https://www.youtube.com/playlist?list=PLofa8IMwGJo1le-DVlYyw93aCc4_Ik3mc');
-            // setTimeout(function () {
-            //     msg.member.voice.channel.leave();
-            // }, 5000);
-        // }
     }
 });
 
